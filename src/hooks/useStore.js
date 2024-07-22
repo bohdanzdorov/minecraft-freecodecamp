b71eb88ff0d299ac241602e482e09ac53698af45
Bohdan Zdorovets <zdorovetsbogdan@gmail.com>
2024-07-22 19:34:45 +0200

    finsihed

--- src/hooks/useStore.js
+++ src/hooks/useStore.js
@@ -1,8 +1,12 @@
 import create from 'zustand'
 import {nanoid} from 'nanoid'
+
+const getLocalStorage = (key) => JSON.parse(window.localStorage.getItem(key))
+const setLocalStorage = (key, value) => window.localStorage.setItem(key, JSON.stringify(value))
+
 export const useStore = create((set) => ({
     texture: 'dirt',
-    cubes: [],
+    cubes: getLocalStorage('cubes') || [],
     addCube: (x, y, z) => {
         set((prev)=>({
             cubes: [
@@ -16,9 +20,26 @@ export const useStore = create((set) => ({
         }))
     },
     removeCube: (x, y, z) => {
-		console.log("Removed")
+		set((prev)=>({
+            cubes: prev.cubes.filter(cube => {
+				const [X, Y, Z] = cube.pos
+				return X != x || Y != y || Z != z
+			})
+        }))
 	},
-    setTexture: () => {},
-    saveWorld: () => {},
-    resetWorld: () => {},
+    setTexture: (texture) => {
+		set(() => ({
+			texture
+		}))
+	},
+    saveWorld: () => {
+		set((prev) => {
+			setLocalStorage('cubes', prev.cubes)
+		})
+	},
+    resetWorld: () => {
+		set(()=> ({
+			cubes: []
+		}))
+	},
 }))
